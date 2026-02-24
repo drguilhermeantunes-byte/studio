d12237329706076fe3a7836031fc87d7e9257c4a
Guilherme Antunes <drguilhermeantunes@gmail.com>
2026-02-24 14:53:15 +0000

    Try fixing this error: `Recoverable Error: Hydration failed because the

--- src/app/tv/components/clock.tsx
+++ src/app/tv/components/clock.tsx
@@ -3,9 +3,10 @@
 import { useState, useEffect } from 'react';
 
 export function Clock() {
-  const [time, setTime] = useState(new Date());
+  const [time, setTime] = useState<Date | null>(null);
 
   useEffect(() => {
+    setTime(new Date());
     const timerId = setInterval(() => {
       setTime(new Date());
     }, 1000);
@@ -15,10 +16,12 @@ export function Clock() {
 
   return (
     <div className="font-mono text-5xl font-bold text-foreground">
-      {time.toLocaleTimeString('pt-BR', {
-        hour: '2-digit',
-        minute: '2-digit',
-      })}
+      {time
+        ? time.toLocaleTimeString('pt-BR', {
+            hour: '2-digit',
+            minute: '2-digit',
+          })
+        : '--:--'}
     </div>
   );
 }
