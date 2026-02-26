ca1baba70b3cab578b779c300037ce7197efb3b1
Guilherme Antunes <drguilhermeantunes@gmail.com>
2026-02-26 18:49:12 +0000

    Try fixing this error: `Runtime ReferenceError: AlertDialogCancel is not

--- src/app/tv/components/tv-display.tsx
+++ src/app/tv/components/tv-display.tsx
@@ -17,6 +17,7 @@ import { Button } from '@/components/ui/button';
 import {
   AlertDialog,
   AlertDialogAction,
+  AlertDialogCancel,
   AlertDialogContent,
   AlertDialogDescription,
   AlertDialogFooter,
